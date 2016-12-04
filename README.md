9ac9ae8ec3703c1e2aa39833410325f4492f387a
b4b4r07 <b4b4r07@gmail.com>
2016-12-04 17:45:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+epoch-cat
+=========
+
+epoch-cat is a filter command that converts the UNIX time appearing in some log files to the human-readable format only for that part
+
+## Installation
+
+```console
+$ go get github.com/b4b4r07/epoch-cat
+```
+
+or
+
+```zsh
+zplug "b4b4r07/epoch-cat", as:command, rename-to:ecat
+```
+
+## Usage
+
+```console
+$ ./epoch-cat -q access.log | jq . | head
+{
+  "time": "2016-05-25T14:35:35Z",
+  "uuid": "3bf99536760ae910dd77a0fb9cc493524dc5b808",
+  "level": "WARNING",
+  "message": {
+    "message": "SQLSTATE[1234] Unknown database 'hyper.services',
+    "_exception": {
+        ...
+        ...
+
+$ echo 1476707810.8115 | ./epoch-cat
+2016-10-17T12:36:50Z
+
+$ echo 1476707810.8115 | TZ=JST ./epoch-cat
+2016-10-17T21:36:50+09:00
+
+$ echo 1476707810.8115 | ./epoch-cat -f "%a, %d %b %Y %H:%M:%S +0000"
+Mon, 17 Oct 2016 12:36:50 +0000
+```
+
+About time format, see also: <https://docs.python.org/2/library/time.html#time.strftime>
+
+## Author
+
+b4b4r07
+
+## License
+
+MIT
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/hhkbp2/go-strftime"
+)
+
+const (
+	unixTimeBegin = 946684800 // 946684800 2000-01-01T00:00:00+0000
+)
+
+var (
+	unixTimeRe = regexp.MustCompile(`([0-9]{9,}(\.[0-9]+)?)`) // e.g. 1476707866.8115
+	// dateTimeRe TODO
+)
+
+var (
+	format = flag.String("f", "", "Specify the time format")
+	quote  = flag.Bool("q", false, "Quote the date to be output")
+)
+
+func init() {
+	var location string
+
+	if os.Getenv("TZ") == "" {
+		location = "UTC"
+	} else {
+		location = os.Getenv("TZ")
+	}
+
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 9*60*60)
+	}
+	time.Local = loc
+}
+
+func doEchoLine(r io.Reader) error {
+	var formattedTime string
+	var buf bytes.Buffer
+
+	unixTimeNow := time.Now().Unix()
+	br := bufio.NewReader(r)
+
+	for {
+		line, isPrefix, err := br.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		if _, err = buf.Write(line); err != nil {
+			return err
+		}
+		if !isPrefix {
+			var matched [][]byte
+			matched = unixTimeRe.FindSubmatch(line)
+			if len(matched) > 0 {
+				matchedUnixTime, err := strconv.ParseFloat(string(matched[0]), 64)
+				if err != nil {
+					fmt.Println(line)
+					return nil
+				}
+				unixTime := int64(matchedUnixTime)
+				if *format == "" {
+					formattedTime = time.Unix(unixTime, 0).Format(time.RFC3339)
+				} else {
+					formattedTime = strftime.Format(*format, time.Unix(unixTime, 0))
+				}
+				if *quote {
+					formattedTime = fmt.Sprintf(`"%s"`, formattedTime)
+				}
+				if unixTimeBegin <= unixTime && unixTime <= unixTimeNow {
+					fmt.Println(unixTimeRe.ReplaceAllString(string(line), formattedTime))
+					return nil
+				}
+			}
+			buf.Reset()
+		}
+	}
+	return nil
+}
+
+func openFile(s string) (io.ReadWriteCloser, error) {
+	fi, err := os.Stat(s)
+	if err != nil {
+		return nil, err
+	}
+	if fi.Mode()&os.ModeSocket != 0 {
+		return net.Dial("unix", s)
+	}
+	return os.Open(s)
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		doEchoLine(os.Stdin)
+	}
+
+	for _, fname := range flag.Args() {
+		if fname == "-" {
+			doEchoLine(os.Stdin)
+		} else {
+			f, err := openFile(fname)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer f.Close()
+			doEchoLine(f)
+		}
+	}
+}
